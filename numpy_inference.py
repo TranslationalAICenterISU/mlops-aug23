667c94f53c8ac9a7c75d3951cad8105fbc5461c9
Aditya Balu <baditya@iastate.edu>
2023-08-18 16:50:10 +0000

    adding more files

--- numpy_inference.py
+++ numpy_inference.py
@@ -3,14 +3,14 @@ import json
 import mlflow
 # Setting a tracking uri to log the mlflow logs in a particular location tracked by 
 from mlflow.tracking import MlflowClient
-tracking_uri = "http://localhost:5000"
+tracking_uri = "http://localhost:5005"
 client = MlflowClient(tracking_uri=tracking_uri)
 mlflow.set_tracking_uri(tracking_uri)
 
-model = mlflow.pyfunc.load_model('file:///mnt/cyverse/mlruns/0/3927b8108e2640219c5ca7801208fd56/artifacts/np_model')
+model = mlflow.pyfunc.load_model('file:///mnt/cyverse/mlruns/0/ab8f1cce2c964e7c8e6d77d864a9da64/artifacts/np_model')
 
 
-np_array = np.random.rand(10,6)
+np_array = np.random.rand(6)
 
 output = model.predict(np_array)
 
